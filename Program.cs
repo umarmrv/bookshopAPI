6f7911da11aef7786abac9be3da24dedd0660d47
Shohjahon <sh.murodovv@gmail.com>
2026-06-24 13:20:41 +0500

    created learning  project of Bookshop Full Api set up program.cs with DI adn Swagger

---
+++ Program.cs
@@ -0,0 +1,41 @@
+
+//dotnet add package Swashbuckle.AspNetCore  --  have to download this for swagger 
+
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// === 1. РЕГИСТРАЦИЯ СЕРВИСОВ (DI КОНТЕЙНЕР) ===
+// Добавляем поддержку контроллеров (аналог Views в Django)
+builder.Services.AddControllers();
+
+
+// Добавляем генератор Swagger (генерация документации API)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// === 2. НАСТРОЙКА MIDDLEWARE PIPELINE (КОНВЕЙЕР) ===
+// Если проект запущен в режиме разработки, включаем Swagger интерфейс
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); // Это создаст красивую страничку по адресу /swagger
+}
+
+
+// Мидлварь для перенаправления с http на https
+app.UseHttpsRedirection();
+
+// Мидлварь маршрутизации (определяет, какой контроллер вызвать)
+app.UseRouting();
+
+// Мидлварь авторизации
+app.UseAuthorization();
+
+// Конец конвейера — маппинг контроллеров к маршрутам
+app.MapControllers();
+
+// Запуск Kestrel сервера
+app.Run();

737297bc01b207def6a85792398b8b801f90ff9e
Shohjahon <sh.murodovv@gmail.com>
2026-06-24 14:17:57 +0500

    installed EF packages and  created models for the Books and Authors with a Db relation One-To Many

---
+++ Models/Author.cs
@@ -0,0 +1,10 @@
+namespace  BookShopCsharp.Models;
+
+public class Author
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    //one author keep many books here is relation one to many 
+    public  List<Book> Books { get; set; }
+}
---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+namespace  BookShopCsharp.Models;
+
+
+public class Book
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public decimal Price { get; set; }
+    
+    public int AuthorID  { get; set; }
+    //setting up models.ForeignKey from author ?? 
+    public Author Author { get; set; }
+    
+}
